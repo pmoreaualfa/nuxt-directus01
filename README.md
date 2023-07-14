0c8f8dbe1c18a0fd415428468303cfc0b163c4ba
Philippe Moreau <fbpmoreau@pm.me>
2023-07-14 21:06:56 +0200

    setup tailwind

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ cd nuxt-directus01
 npm run dev
 # nuxt-directus
 npm install --save-dev nuxt-directus
+# tailwind
+npm install --save-dev @nuxtjs/tailwindcss
+assets/css/tailwind.css
+
+# content
+
+# pinia
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['nuxt-directus'],
+  modules: ['nuxt-directus','@nuxtjs/tailwindcss'],
   directus: {
     url: ""
+  },
+  tailwindcss: {
+    // Options
   }
 })
